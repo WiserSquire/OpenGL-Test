41a284a6a969592605688ac47d5136c17b073848
WiserSquire <jacobdaniel0603@gmail.com>
2023-01-20 16:02:37 -0500

    Lighting Maps

--- src/shader/colors.fs
+++ src/shader/colors.fs
@@ -1,8 +1,8 @@
 #version 330 core
 struct Material{
-    vec3 ambient;
-    vec3 diffuse;
-    vec3 specular;
+    sampler2D diffuse;
+    sampler2D specular;
+    sampler2D emission;
     float shininess;
 };
 
@@ -16,6 +16,7 @@ struct Light{
 
 in vec3 Normal;
 in vec3 FragPos;
+in vec2 TexCoords;
 
 out vec4 FragColor;
 
@@ -26,20 +27,28 @@ uniform Light light;
 void main()
 {
     // ambient
-    vec3 ambient = light.ambient * material.ambient;
-  	
+    vec3 ambient = light.ambient * vec3(texture(material.diffuse, TexCoords)); 
+     	
     // diffuse 
     vec3 norm = normalize(Normal);
     vec3 lightDir = normalize(light.position - FragPos);
     float diff = max(dot(norm, lightDir), 0.0);
-    vec3 diffuse = light.diffuse * (diff * material.diffuse);
+    vec3 diffuse = light.diffuse * diff * vec3(texture(material.diffuse, TexCoords));
     
     // specular
     vec3 viewDir = normalize(viewPos - FragPos);
     vec3 reflectDir = reflect(-lightDir, norm);  
     float spec = pow(max(dot(viewDir, reflectDir), 0.0), material.shininess);
-    vec3 specular = light.specular * (spec * material.specular);  
+    vec3 specular = light.specular * spec * vec3(texture(material.specular, TexCoords));
+
+    // emission
+    vec3 emission;
+    if (vec3(texture(material.specular, TexCoords)) == vec3(0.0)){
+        emission = vec3(texture(material.emission, TexCoords));
+    } else {
+        emission = vec3(0.0);
+    }
         
-    vec3 result = ambient + diffuse + specular;
+    vec3 result = ambient + diffuse + specular + emission;
     FragColor = vec4(result, 1.0);
 } 
